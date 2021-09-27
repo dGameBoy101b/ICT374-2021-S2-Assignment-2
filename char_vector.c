f647c1ecda1b818462cb77519703ac0e923647f9
dGameBoy101b <rhysmader@gmail.com>
2021-09-27 15:24:00 +0800

    Char Vec Improvements
    Added capacity management functionality to CharVec module.
    Improved and corrected some documentation in CharVec module.
    Added copying functionality to CharVec module.
    Stripped unnecessary countCharVec function from CharVec module.
    Added C-string extraction function to CharVec module.

--- char_vector.c
+++ char_vector.c
@@ -3,9 +3,11 @@
 struct CharVec* createCharVec()
 {
 	struct CharVec* vec = (struct CharVec*)malloc(sizeof(struct CharVec));
-	if (vec == NULL) return;
+	if (vec == NULL) return NULL;
 	vec->count = 0;
+	vec->capacity = 1;
 	vec->vec = malloc(sizeof(char));
+	return vec;
 }
 void destroyCharVec(struct CharVec** vec)
 {
@@ -14,21 +16,43 @@ void destroyCharVec(struct CharVec** vec)
 	free(*vec);
 	*vec = NULL;
 }
-unsigned int countCharVec(struct CharVec* vec)
+int copyCharVec(struct CharVec* dst, struct CharVec* src)
 {
-	return vec->count;
+	if (dst == NULL || src == NULL) return 0;
+	if (!reserveCharVec(dst, src->count)) return 0;
+	for (unsigned int i = 0; i < src->count; ++i) dst->vec[i] = src->vec[i];
+	dst->count = src->count;
+	return 1;
 }
 char* getEleCharVec(struct CharVec* vec, unsigned int index)
 {
-	if (vec == NULL || index >= countCharVec(vec)) return NULL;
+	if (vec == NULL || index >= vec->count) return NULL;
 	return vec->vec + index;
+}
+int getStrCharVec(char** str, struct CharVec* vec)
+{
+	if (str == NULL || vec == NULL) return 0;
+	char* cha = realloc(*str, sizeof(char) * (vec->count + 1));
+	if (cha == NULL) return 0;
+	*str = cha;
+	for (unsigned int i = 0; i < vec->count; ++i) (*str)[i] = vec->vec[i];
+	(*str)[vec->count] = '\0';
+	return 1;
+}
+int reserveCharVec(struct CharVec* vec, unsigned int cap)
+{
+	if (vec == NULL) return 0;
+	if (cap <= vec->capacity) return 1;
+	char* cha = realloc(vec->vec, sizeof(char) * cap);
+	if (cha == NULL) return 0;
+	vec->vec = cha;
+	vec->capacity = cap;
+	return 1;
 }
 int appendEleCharVec(struct CharVec* vec, char c)
 {
 	if (vec == NULL) return 0;
-	char* cha = realloc(vec->vec, sizeof(char) * (countCharVec(vec) + 2));
-	if (cha == NULL) return 0;
-	vec->vec = cha;
+	if (vec->count + 1 > vec->capacity) if(!reserveCharVec(vec, vec->count + 1)) return 0;
 	vec->vec[vec->count] = c;
 	++(vec->count);
 	return 1;
@@ -36,10 +60,12 @@ int appendEleCharVec(struct CharVec* vec, char c)
 int clearCharVec(struct CharVec* vec)
 {
 	if (vec == NULL) return 0;
-	if (countCharVec(vec) == 0) return 1;
-	char* cha = realloc(vec->vec, sizeof(char));
+	if (vec->count == 0) return 1;
+	char* cha = malloc(sizeof(char));
 	if (cha == NULL) return 0;
+	free(vec->vec);
 	vec->vec = cha;
 	vec->count = 0;
+	vec->capacity = 1;
 	return 1;
 }
--- char_vector.h
+++ char_vector.h
@@ -1,10 +1,11 @@
 struct CharVec {
-	char* vec;
-	unsigned int count;
+	char* vec; ///The dynamic array containing all elements
+	unsigned int count; ///The number of elements on the vector
+	unsigned int capacity; ///The number of elements this vector can currently hold
 };
 
 /** Create a new character vector
-\param vec The new character vector
+\return The new character vector (NULL if failed)
 */
 struct CharVec* createCharVec();
 
@@ -13,18 +14,33 @@ struct CharVec* createCharVec();
 */
 void destroyCharVec(struct CharVec** vec);
 
-/** Get the number of elements in the given character vector
-\param vec The character vector to measure
-\return The number of elements in the given character vector
+/** Copy the source character vector into the destination character vector
+\param dst The destination character vector to copy into
+\param src The source character vector to copy from
+\return 0 on failure, 1 on success
 */
-unsigned int countCharVec(struct CharVec* vec);
+int copyCharVec(struct CharVec* dst, struct CharVec* src);
 
 /** Get an element from the given character vector
 \param vec The character vector to search
 \param index The index at which to find the element
-\return A pointer to the found element
+\return A pointer to the found element (NULL if out of range)
 */
 char* getEleCharVec(struct CharVec* vec, unsigned int index);
+
+/** Copy the contents of the given character vector into the given C-string
+\param str A pointer to the C-string to fill
+\param vec The character vector the copy from
+\return 0 on failure, 1 on success
+*/
+int getStrCharVec(char** str, struct CharVec* vec);
+
+/** Reserve at least the given capacity in the given character vector
+\param vec The character vector to reserve space for
+\param cap The minimum number of elements the given vector must be able to store
+\return 0 on failure, 1 on success
+*/
+int reserveCharVec(struct CharVec* vec, unsigned int cap);
 
 /** Append an element to the given character vector
 \param vec The character vector to append to
