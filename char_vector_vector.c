5639a1dee1a49207196191b5a43a3063d49eb0f1
dGameBoy101b <rhysmader@gmail.com>
2021-09-29 16:13:02 +0800

    Created Character Vector Vector
    Created a vector that can hold character vectors.
    Currently untested.

---
+++ char_vector_vector.c
@@ -0,0 +1,87 @@
+#include "char_vector_vector.h"
+struct CharVecVec* createCharVecVec()
+{
+	struct CharVecVec* vec = malloc(sizeof(struct CharVecVec));
+	if (vec == NULL)
+		return NULL;
+	struct CharVec* cha = malloc(sizeof(struct CharVec));
+	if (cha == NULL)
+	{
+		free(vec);
+		return NULL;
+	}
+	vec->vec = cha;
+	vec->count = 0;
+	vec->capacity = 1;
+	return vec;
+}
+void destroyCharVecVec(struct CharVecVec**const vec)
+{
+	if (vec == NULL || *vec == NULL)
+		return;
+	for (unsigned int i = 0; i < (*vec)->count; ++i)
+		destroyCharVec(&((*vec)->vec + i));
+	free((*vec)->vec);
+	free(*vec);
+	*vec = NULL;
+}
+int reserveCharVecVec(struct CharVecVec*const vec, unsigned int cap)
+{
+	if (vec == NULL)
+		return 0;
+	if (cap <= vec->capacity)
+		return 1;
+	struct CharVec* cha = realloc(vec->vec, sizeof(struct CharVec) * cap);
+	if (cha == NULL)
+		return 0;
+	vec->vec = cha;
+	vec->capacity = cap;
+	return 1;
+}
+int copyCharVecVec(struct CharVecVec*const dst, const struct CharVecVec*const src)
+{
+	if (dst == NULL || src == NULL || !reserveCharVecVec(dst, src->count))
+		return 0;
+	for (unsigned int i = 0; i < src->count; ++i)
+		if (!copyCharVec(dst->vec + i, src->vec + i))
+			return 0;
+	dst->count = src->count;
+	return 1;
+}
+int equalCharVecVec(const struct CharVecVec*const u, const struct CharVecVec*const v)
+{
+	if (u == NULL || v == NULL || u->count != v->count)
+		return 0;
+	for (unsigned int i = 0; i < v->count; ++i)
+		if (!equalCharVec(u->vec + i, v->vec + i))
+			return 0;
+	return 1;
+}
+struct CharVec* getEleCharVecVec(struct CharVecVec*const vec, unsigned int index)
+{
+	if (vec == NULL || index >= vec->count)
+		return NULL;
+	return vec->vec + index;
+}
+int appendEleCharVecVec(struct CharVecVec*const vec, const struct CharVec*const ele)
+{
+	if (vec == NULL || ele == NULL || !reserveCharVecVec(vec, vec->count + 1) || !copyCharVec(vec->vec + vec->count, ele))
+		return 0;
+	(vec->count)++;
+	return 1;
+}
+int clearCharVecVec(struct CharVecVec*const vec)
+{
+	if (vec == NULL)
+		return 0;
+	struct CharVec* cha = malloc(sizeof(struct CharVec));
+	if (cha == NULL)
+		return 0;
+	for (unsigned int i = 0; i < vec->count; ++i)
+		destroyCharVec(&(vec->vec + i));
+	free(vec->vec);
+	vec->vec = cha;
+	vec->capacity = 1;
+	vec->count = 0;
+	return 1;
+}
---
+++ char_vector_vector.h
@@ -0,0 +1,63 @@
+#ifndef CHAR_VECTOR_VECTOR_H
+#define CHAR_VECTOR_VECTOR_H
+
+#include "char_vector.h"
+
+struct CharVecVec {
+	struct CharVec* vec; ///The dynamic array of character vectors
+	unsigned int count; ///The number of character vectors in this vector
+	unsigned int capacity; ///The number of character vectors this vector can hold
+};
+
+/** Create an empty vector of character vectors
+\return A pointer to the created vector of character vectors (NULL on failure)
+*/
+struct CharVecVec* createCharVecVec();
+
+/** Destroy the given vector of character vectors
+\param vec The vector to destroy
+*/
+void destroyCharVecVec(struct CharVecVec**const vec);
+
+/** Reserve more space in the given vector for the given minimum capacity
+\param vec The vector of character vectors to expand
+\param cap The minimum required capacity
+\return 0 on failure, 1 on success
+*/
+int reserveCharVecVec(struct CharVecVec*const vec, unsigned int cap);
+
+/** Copy the source vector into the destination vector
+\param dst The vector of character vectors to copy into
+\param src The vector of character vectors to copy from
+\return 0 on failure, 1 on success
+*/
+int copyCharVecVec(struct CharVecVec*const dst, const struct CharVecVec*const src);
+
+/** Test if the given vectors of character vectors are equal
+\param u A vector to compare
+\param v The other vector to compare
+\return 1 if equal, 0 if not equal
+*/
+int equalCharVecVec(const struct CharVecVec*const u, const struct CharVecVec*const v);
+
+/** Get an element from the given vector of character vectors
+\param vec The vector to search
+\param index The index of the element to retrieve
+\return A pointer to the found element (NULL if not found)
+*/
+struct CharVec* getEleCharVecVec(struct CharVecVec*const vec, unsigned int index);
+
+/** Append the given element to the end of the given vector
+\param vec The vector to append to the end of
+\param ele The character vector to append
+\return 0 on failure, 1 on success
+*/
+int appendEleCharVecVec(struct CharVecVec*const vec, const struct CharVec*const ele);
+
+/** Clear the given vector of all elements
+\param vec The vector to clear
+\return 0 on failure, 1 on success
+*/
+int clearCharVecVec(struct CharVecVec*const vec);
+
+#endif // CHAR_VECTOR_VECTOR_H
