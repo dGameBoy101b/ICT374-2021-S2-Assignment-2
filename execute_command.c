8f9087cf0e6aef9406b527cf44187dcdea2ef181
dGameBoy101b <rhysmader@gmail.com>
2021-10-20 15:47:56 +0800

    Update execute_command.c
    Added clean up of temporary string.
    Completed change prompt functionality.

--- execute_command.c
+++ execute_command.c
@@ -1,5 +1,6 @@
 #include "execute_command.h"
 #include "PWDFuncs.h"
+#include "change_prompt.h"
 #include <stdlib.h>
 
 void executeCommand(const struct Command*const com)
@@ -7,7 +8,8 @@ void executeCommand(const struct Command*const com)
 	if (com == NULL)
 		exit(-1);
 	//check for special commands
-	char* path = NULL;
+	char* str = NULL;
+	int res;
 	if (equalCharVec(com->path, PWD_KEYWORD)
 	{
 		PrintPWD();
@@ -15,28 +17,40 @@ void executeCommand(const struct Command*const com)
 	}
 	if (equalCharVec(com->path, CD_KEYWORD))
 	{
-		if (com->args->count < 2 || !getStrCharVec(path, com->args->vec + 1) || !DirectoryWalk(path))
-			exit(-1);
-		exit(0);
+		res = com->args->count < 2 || !getStrCharVec(str, com->args->vec + 1) || !DirectoryWalk(str);
+		free(str);
+		if (res)
+			exit(0);
+		exit(-1);
 	}
 	if (equalCharVec(com->path, PROMPT_KEYWORD))
 	{
-		//change prompt file
-		exit(0);
+		res = com->args->count < 2 || !getStrCharVec(str, com->args->vec + 1) || !changePrompt(str);
+		free(str);
+		if (res)
+			exit(0);
+		exit(-1);
 	}
-	if (!getStrCharVec(path, com->path))
+	if (!getStrCharVec(str, com->path))
 		exit(-1);
 	char** argv = malloc(sizeof(char*) * (com->args->count + 1));
 	if (argv == NULL)
+	{
+		free(str);
 		exit(-1);
+	}
 	argv[com->args->count] = NULL;
 	char* arg = NULL;
 	for (unsigned int i = 0; i < com->args->count; ++i)
 	{
 		argv[i] = NULL;
 		if (!getStrCharVec(argv[i], com->args->vec[i]))
+		{
+			free(str);
 			exit(-1);
+		}
 	}
-	execv(path, argv);
+	execv(str, argv);
+	free(str);
 	exit(-1);
 }
